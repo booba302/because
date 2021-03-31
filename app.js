c2b25f28ca2e3e64dcbc1715b6bbdf121bb2f26a
Carol Mirabal <carolmirabal27@gmail.com>
2021-03-30 23:30:58 -0300

    cambios generales

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'; // Archivo CSS de Bootstrap 4 
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 
 const App = () => (
   <div></div>
 )
 
-export default App
+if (document.getElementById('app')) {
+    ReactDOM.render(<App />, document.getElementById('app'))
+}
